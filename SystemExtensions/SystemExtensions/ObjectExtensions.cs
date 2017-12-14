49f577e517f28dcbcd8eb21173dae9e07af8fca0
Julian Walls <j_walls@hotmail.co.uk>
2017-12-14 07:05:43 +0000

    SystemExtensions added with initial object extensions

---
+++ SystemExtensions/SystemExtensions/ObjectExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Walls.Julian.System.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsNull<T>(this T @object) where T : class
+        {
+            return @object == null;
+        }
+
+        public static bool IsNotNull<T>(this T @object) where T : class
+        {
+            return !@object.IsNull();
+        }
+    }
+}
---
+++ SystemExtensions/Tests/Unit/SystemExtensions.Tests.Unit/ObjectExtensionsFixture.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+
+namespace Walls.Julian.System.Extensions.Tests.Units
+{
+    [TestFixture]
+    internal class ObjectExtensionsFixture
+    {
+        [Test]
+        public void IsNull_ReturnsTrue_WhenObjectIsNull()
+        {
+            //Arrange
+            TestObject @object = null;
+
+            //Act and Assert
+            Assert.That(@object.IsNull(), Is.True);
+        }
+
+        [Test]
+        public void IsNull_ReturnsFalse_WhenObjectIsNotNull()
+        {
+            //Arrange
+            TestObject @object = new TestObject();
+
+            //Act and Assert
+            Assert.That(@object.IsNull(), Is.False);
+        }
+
+        [Test]
+        public void IsNotNull_ReturnsTrue_WhenObjectIsNotNull()
+        {
+            //Arrange
+            TestObject @object = new TestObject();
+
+            //Act and Assert
+            Assert.That(@object.IsNotNull(), Is.True);
+        }
+
+        [Test]
+        public void IsNotNull_ReturnsFalse_WhenObjectIsNull()
+        {
+            //Arrange
+            TestObject @object = null;
+
+            //Act and Assert
+            Assert.That(@object.IsNotNull(), Is.False);
+        }
+
+        private class TestObject
+        {
+
+        }
+    }
+}
